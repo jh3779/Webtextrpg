b28160d3699fdff8c58d5bf3399485b8ec8f2ede
Seongyul-Lee <tjddbfzsd66@gmail.com>
2026-03-23 20:12:31 +0900

    feat: 3주차 게임 로직 함수 정의 완료

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
-// [1주차 과제] 아래 변수들에 본인의 캐릭터 정보를 입력해 보세요!
-
 let characterName = "신입 궁수"; 
-// let: 변수(상자)를 선언하는 예약어로, 나중에 상자 안의 값을 바꿀 수 있게 해줍니다.
-// characterName: 메모리에 저장된 데이터에 접근하기 위한 식별자(이름)입니다.
-// "신입 모험가": 문자열(String) 데이터 타입으로, 텍스트 정보를 저장할 때 반드시 따옴표를 씁니다.
 
 const initialLevel = 1;
-// const: '상수'를 선언하며, 한 번 할당한 값을 절대 바꿀 수 없도록 보호하는 키워드입니다.
-// 1: 숫자(Number) 데이터 타입으로, 산술 연산이 가능한 데이터입니다.
-
-// 화면에 캐릭터 이름을 표시하는 로직 (4주차에 자세히 배웁니다)
-document.getElementById("p-name").innerText = characterName;
-// document: 현재 실행 중인 HTML 문서 전체를 가리키는 객체입니다.
-// getElementById("p-name"): HTML 코드 중 id가 "p-name"인 특정 태그를 찾아오는 함수입니다.
-// .innerText: 찾은 태그 안에 들어갈 텍스트 내용을 변경하는 속성입니다.
-
-console.log("게임 준비 완료: " + characterName);
-// console.log: 브라우저 개발자 도구(F12)의 콘솔 창에 메시지를 출력하여 버그를 찾거나 확인하는 도구입니다.
 
 const player = {
   hp: 100, 
@@ -26,16 +10,68 @@ const player = {
 // 플레이어 객체
 
 const monsters = [
-  {name: "슬라임", hp: 30, atk: 20},
-  {name: "고블린", hp: 120, atk: 12},
-  {name: "드래곤", hp: 180, atk: 20}
+  {name: "슬라임", hp: 30, maxhp: 30,atk: 20},
+  {name: "고블린", hp: 120, maxhp: 120, atk: 12},
+  {name: "드래곤", hp: 180, maxhp:180, atk: 20}
   ]
 // 몬스터 3마리를 배열로 선언
 
 let currentFloor = 1;
+const maxFloor = 3;
 // 현재 몇 층인지 추적하는 던전 상태 변수
 
-console.log("체력: ", + player.hp)
-console.log("최대 체력: ", + player.maxHp)
-console.log("공격력: ", + player.atk)
-console.log("1층 몬스터: " + monsters[currentFloor - 1].name)
+function playerAttack(monster) { 
+  monster.hp -= player.atk;
+
+  if (monster.hp < 0) {
+    monster.hp = 0;
+  }
+}
+// 플레이어 → 몬스터 공격 처리
+
+function monsterAttack(monster) {
+  player.hp -= monster.atk;
+
+  if (player.hp < 0) {
+    player.hp = 0;
+  }
+}
+// 몬스터 → 플레이어 공격 처리
+
+function checkGameOver() {
+  return player.hp <= 0;
+}
+// 게임오버 판정
+
+function healAfterClear() {
+  const healAmount = [10, 20, 30];
+  player.hp += healAmount[currentFloor - 1];
+
+  if (player.hp > player.maxHp) {
+    player.hp = player.maxHp;
+  }
+}
+// 층 클리어 시 HP 회복
+// 층마다 다른 회복량 적용, maxHp 초과 불가
+
+function checkMonsterDead() {
+  return monsters[currentFloor - 1].hp <= 0;
+}
+// 현재 층의 몬스터가 죽었는지 판정
+
+function nextFloor() {
+  currentFloor += 1;
+}
+// 다음 층으로 이동
+// 몬스터 처치 → healAfterClear() → nextFloor() - Clear했던 층의 회복량 적용
+// 몬스터 처치 → nextFloor() → healAfterClear() ❌ - 다음 층 회복량이 적용됨
+
+function checkGameClear() {
+  return monsters[maxFloor - 1].hp <= 0;
+}
+// 마지막 층까지 Clear 한 건지 확인
+
+document.getElementById("p-name").innerText = characterName;
+// document: 현재 실행 중인 HTML 문서 전체를 가리키는 객체입니다.
+// getElementById("p-name"): HTML 코드 중 id가 "p-name"인 특정 태그를 찾아오는 함수입니다.
+// .innerText: 찾은 태그 안에 들어갈 텍스트 내용을 변경하는 속성입니다.
